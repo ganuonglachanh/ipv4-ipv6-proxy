f84bdc1b013cb919b21fc73c1a7c892efe985919
ganuonglachanh <ganuonglachanh@outlook.com>
2020-04-05 13:16:33 +0700

    Update Centos 8

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,16 +1,17 @@
 echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+yum -y install gcc make wget net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
+DEFAULTNET=$(ip -o -4 route show to default | awk '{print $5}')
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s ifconfig.co)
 IP6=$(curl -6 -s ifconfig.co | cut -f1-4 -d':')
-
+echo "Default net interface = ${DEFAULTNET}"
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 echo "How many proxy do you want to create? Example 500"
@@ -22,14 +23,20 @@ LAST_PORT=$(($FIRST_PORT + $COUNT))
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/sysctl.conf <<EOF
+fs.file-max=500000
+EOF
+
+sysctl -w fs.file-max=500000
 
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 2048
+ulimit -n 65536
 service 3proxy start
 EOF
 
@@ -37,4 +44,7 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-upload_proxy
+zip proxy.zip proxy.txt
+echo "Done! ${WORKDATA}"
+
+#upload_proxy
